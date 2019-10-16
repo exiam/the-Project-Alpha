6caf99b9bb7045114532692290b756758851ce79
Hugo_s29 <y.hugo.s29@gmail.com>
2019-10-16 16:56:36 +0200

    Modify to pass tests II

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,7 @@ module.exports = {
     },
   },
   setupFiles: ['<rootDir>/setupTests.js'],
+  setupTestFrameworkScriptFile: './jest.setup.js',
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
